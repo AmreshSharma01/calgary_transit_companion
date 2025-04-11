bf1cfc905182f4cba88d4e0e604955df2c6a6867
Ujjal Baniya <ujjalbaniya@gmail.com>
2025-04-10 21:02:46 -0600

    adding database uri and GTFS Directory in config

--- config.py
+++ config.py
@@ -2,3 +2,9 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
+
+DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///transit.db')
+
+GTFS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'gtfs')
